5457414d9661f350713c38a32e5f002ad1940854
Timothy McKenna <mckenna.tim@gmail.com>
2014-04-28 16:53:56 -0400

    move model creation to factory

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 var url='http://10.0.1.24:3030/';
-var app = angular.module('app', ['ngRoute']);
+
+var app = angular.module('app', []);
 
 app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
@@ -7,51 +8,88 @@ app.config(['$httpProvider', function($httpProvider) {
   }
 ]);
 
-function Ctrl1Ctrl($scope, $http){
-  $scope.getout = '';
+
+var RestData = function($http){
+  this.get= function(){
+    var promise = $http.get(url +'animals/').then(function(data) {
+      console.log(data.data);
+      return data;
+    });
+    return promise;    
+  }
+  this.post = function(){
+    var promise=$http.post(url+'animals/', {type:'duck',name:'daffy'}).then(function(data) {
+      console.log(data.data);
+      return data;
+    });
+    return promise;    
+  }
+  this.put = function(){
+    var type = 'cat';
+    var newname = {name:'snowball'};
+    var promise=$http.put(url+'animals/'+type, newname).then(function(data) {
+      console.log(data.data);
+      return data;
+    });
+    return promise;    
+  } 
+  this.del = function(){
+    var type = 'duck';
+    var name = {name:'daffy'};
+    var promise=$http.delete(url+'animals/'+type, name).then(function(data) {
+      console.log(data.name);
+      return data;
+    });
+    return promise;    
+  }    
+  this.cow = 'mycaw';
+  this.rec = {dog:'mabibi'};
+  return  this;  
+}
+app.factory('AnimalData', RestData);
+
+/* is the same as the  following anonymous version
+app.factory('AnimalData', function($http){
+  this.get= function(){
+    var promise = $http.get(url +'animals/').then(function(data) {
+      console.log(data.data);
+      return data;
+    });
+    return promise;    
+  }
+
+  this.cow = 'mycaw';
+  this.rec = {dog:'mabibi'};
+  return  this;
+});
+*/
+
+app.controller('Ctrl1Ctrl', function($scope, AnimalData){
+  $scope.getout = "";
   $scope.postout = '';
   $scope.putout = '';
   $scope.delout = '';
   $scope.dog='Butler the mutt';
   $scope.get = function(){
-    var promise=$http.get(url+'animals').then(function(data) {
-      console.log(data.data[0])
-      $scope.getout= data.data;
-      return data;
+    AnimalData.get().then(function(d){
+      $scope.getout = d.data;    
     });
-    return promise;    
   }
   $scope.post = function(){
-    var promise=$http.post(url+'animals/', {type:'duck',name:'daffy'}).then(function(data) {
-      console.log(data.data);
-      $scope.postout= data.data;
-      return data;
+    AnimalData.post().then(function(d){
+      $scope.postout = d.data;    
     });
-    return promise;    
-  }  
-  $scope.put = function(){
-    var type = 'cat';
-    var newname = {name:'snowball'};
-    var promise=$http.put(url+'animals/'+type, newname).then(function(data) {
-      console.log(data.data);
-      $scope.putout= data.data;
-      return data;
-    });
-    return promise;    
-  }  
-  $scope.del = function(){
-    var type = 'duck';
-    var name = {name:'daffy'};
-    var promise=$http.delete(url+'animals/'+type, name).then(function(data) {
-      console.log(data.name);
-      $scope.delout= data.name;
-      return data;
-    });
-    return promise;    
   }
-}
-
-var appServices = angular.module('appServices', []);
-appServices.factory('ItemsData', function($http) {
-  
+  $scope.put = function(){
+    AnimalData.put().then(function(d){
+      $scope.putout = d.data;    
+    });
+  }
+  $scope.del = function(){
+    AnimalData.del().then(function(d){
+      $scope.delout = d.data;    
+    });
+  }
+  $scope.postout ="duck"
 });
+
